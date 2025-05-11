bd555d960f4f6f6d17d7fb91d6c1b0d71d2641de
lovexiaoxia <aadyx2007@163.com>
2025-05-11 10:45:21 +0800

    first commit

---
+++ Example/MKLoRaDeviceModePageModule/MKMotionModePageProtocol.h
@@ -0,0 +1,13 @@
+//
+//  MKMotionModePageProtocol.h
+//  MKLoRaDeviceModePageModule_Example
+//
+//  Created by aa on 2025/5/11.
+//  Copyright © 2025 lovexiaoxia. All rights reserved.
+//
+
+#ifndef MKMotionModePageProtocol_h
+#define MKMotionModePageProtocol_h
+
+
+#endif /* MKMotionModePageProtocol_h */
